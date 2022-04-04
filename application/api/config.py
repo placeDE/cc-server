1f68f5d26a14c54259e19c079d59a395789bbe38
Jan Max Tiedemann <janmaxtiedemann@aol.com>
2022-04-05 00:02:56 +0200

    rotate token and remove old

--- application/api/config.py
+++ application/api/config.py
@@ -5,7 +5,7 @@ class ServerConfig(BaseSettings):
     remote_config_url: str = 'https://placede.github.io/pixel/pixel.json'
     canvas_update_interval: int = 10
     admin_password: str = "bea976c455d292fdd15256d3263cb2b70f051337f134b0fa9678d5eb206b4c45ebd213694af9cf6118700fc8488809be9195c7eae44a882c6be519ba09b68e47"
-    influx_token: str = "b04zjucB13Ho5hjkzDsJX3E2at1n8RXHbwoQIHst"
+    influx_token: str 
     influx_url: str = "https://influx.place.dertiedemann.com"
 
 def get_graphql_config():
