a6c38d7fbc53be6724676d0736487285709510ab
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2022-04-04 17:56:51 +0200

    improved times

--- application/api/connection_manager.py
+++ application/api/connection_manager.py
@@ -1,30 +1,30 @@
-from typing import List
+from typing import List, Tuple
 
 from fastapi import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] = []
+        self.active_connections: List[Tuple[WebSocket, str]] = []
         self.advertised_accounts: dict = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, uuid: str, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections.append((websocket, uuid))
 
-    def set_advertised_accounts(self, websocket: WebSocket, count):
-        self.advertised_accounts[websocket] = count
+    def set_advertised_accounts(self, uuid: str, count):
+        self.advertised_accounts[uuid] = count
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-        self.advertised_accounts.pop(websocket)
+    def disconnect(self, uuid: str, websocket: WebSocket):
+        self.active_connections.remove((websocket, uuid))
+        self.advertised_accounts.pop(uuid)
 
     async def send_message_to(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection[0].send_text(message)
 
     def connection_count(self):
         return len(self.active_connections)
